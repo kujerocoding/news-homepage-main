ede806138d1529dc8fd8dc4167d50f19043a7244
herom <jerome.manset22@gmail.com>
2023-02-09 02:01:10 +0800

    add Attribution component to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Main from './components/Main'
+import Attribution from './components/Attribution'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Main />
+      <Attribution />
     </div>
   )
 }
