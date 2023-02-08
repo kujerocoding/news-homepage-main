01340e575d03e04edd93afab29a75d2ceb4f2410
herom <jerome.manset22@gmail.com>
2023-02-09 02:05:06 +0800

    add images to the Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -24,19 +24,19 @@ const Main = () => {
       </div>
       <div>
         <div>
-            <img src="" alt="" />
+            <img src="../src/assets/images/image-retro-pcs.jpg" alt="" />
             <h2>01</h2>
             <h3>Reviving Retro PCs</h3>
             <p>What happens when old PCs are given modern upgrades?</p>
         </div>
         <div>
-            <img src="" alt="" />
+            <img src="../src/assets/images/image-top-laptops.jpg" alt="" />
             <h2>02</h2>
             <h3>Top 10 Laptops of 2022</h3>
             <p>Our best picks for various needs and budgets.</p>
         </div>
         <div>
-            <img src="" alt="" />
+            <img src="../src/assets/images/image-gaming-growth.jpg" alt="" />
             <h2>03</h2>
             <h3>The Growth of Gaming</h3>
             <p>How the pandemic has sparked fresh opportunities.</p>
