79162c9696c435d945a5b4941daff0d8e98e8d6d
herom <jerome.manset22@gmail.com>
2023-02-09 01:59:09 +0800

    add Attribution component

---
+++ src/components/Attribution.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Attribution = () => {
+  return (
+    <div>
+      Challenge by <a href="https://www.frontendmentor.io/home" target="_blank">Frontend Mentor.</a> Coded by kujerocoding
+    </div>
+  )
+}
+
+export default Attribution
