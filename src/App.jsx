6d216b8e844d953cbdc1013b6a38e56ec9e38c93
herom <jerome.manset22@gmail.com>
2023-02-09 03:09:23 +0800

    add menuIsOn state and toggle function then pass the props to Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import Navbar from './components/Navbar'
 import Main from './components/Main'
 import Attribution from './components/Attribution'
 import './App.css'
+import {useState} from 'react'
 
 const App = () => {
+
+  const [menuIsOn, setMenuIsOn] = useState(false)
+
+  function toggle(){
+    setMenuIsOn(prevMenu => !prevMenu)
+
+  }
+
   return (
     <div>
-      <Navbar />
+      <Navbar menuIsOn={menuIsOn} handleClick={toggle}/>
       <Main />
       <Attribution />
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,17 @@ import IconMenuClose from '../assets/images/icon-menu-close.svg'
 import Logo from '../assets/images/logo.svg'
 
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const style = {
+        display: props.menuIsOn ? "flex" : "none"
+    }
   return (
     <div>
         <nav>
-            <img src={Logo} alt="" className='nav-logo' />
-            <img src={IconMenu} alt="" />
+            <img src={Logo} alt="" className='nav-logo'  />
+            <img src={IconMenu} alt="" onClick={props.handleClick} />
         </nav>
-        <div className='menu-active'>
+        <div className='menu-active' style={style} onClick={props.handleClick}>
             <img src={IconMenuClose} alt="" />
             <ul>
                 <li>Home</li>
