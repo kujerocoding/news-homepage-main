081a00ca243ca8de1f67230a8ac67bde27be021b
herom <jerome.manset22@gmail.com>
2023-02-09 01:38:07 +0800

    add Navbar and Main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
 
 const App = () => {
   return (
     <div>
-      News homepage
+      <Navbar />
+      <Main />
     </div>
   )
 }
